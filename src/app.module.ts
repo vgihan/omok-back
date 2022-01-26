57ec0e156469bcf24941bf27afd9cc68765ec053
vgihan <jum654@naver.com>
2022-01-26 17:39:56 +0900

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), UserModule],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
