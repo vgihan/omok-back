ddc438cf8d6af58bc829d823e953718a4458ff1e
vgihan <jum654@naver.com>
2022-02-18 01:29:38 +0900

    fix: remove test request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -14,10 +14,5 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signIn(authCredentialsDto);
-  }
-
-  @Post('/test')
-  test(@Req() req) {
-    console.log('req', req);
   }
 }
