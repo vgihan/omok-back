a218bc29893c09f55240fb3cead0771083f140bb
vgihan <jum654@naver.com>
2022-02-17 23:16:38 +0900

    feat: add AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
 import { Connection } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule],
 })
 export class AppModule {
   constructor(private connection: Connection) {}
