399e29ba3cef1936ae92af06e99835eed078cc80
vgihan <jum654@naver.com>
2022-03-14 16:57:10 +0900

    fix: 쿠키 설정 로직 삭
    - client로 token을 전달해주고 클라이언트에서 쿠키를 설정하도록 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { UserRepository } from './user.repository';
+import { UserRepository } from '../user/user.repository';
 import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
@@ -25,15 +25,14 @@ export class AuthService {
     const { id, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ id });
 
-    if (user && (await compare(password, user.password))) {
-      const payload = { id };
-      const token = this.jwtService.sign(payload, {
-        secret: this.configService.get<string>('JWT_SECRET'),
-      });
-
-      return { token };
-    } else {
+    if (!user || !(await compare(password, user.password)))
       throw new UnauthorizedException('로그인에 실패하였습니다.');
-    }
+
+    const payload = { id };
+    const token = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+    });
+
+    return { token };
   }
 }
