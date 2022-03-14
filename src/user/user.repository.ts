0c68eae2592e4f07b4d6f3f8c0656342c82528bb
vgihan <jum654@naver.com>
2022-03-14 17:16:01 +0900

    fix: user entity, repository 위치 이동
    - auth to user

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,6 +1,6 @@
 import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthCredentialsDto } from '../auth/dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { hash, genSalt } from 'bcrypt';
 
@@ -10,18 +10,12 @@ export class UserRepository extends Repository<User> {
     const { id, password } = authCredentialsDto;
     const salt = await genSalt();
     const encryptedPassword = await hash(password, salt);
+    const keys = ['id', 'password', 'rating', 'win_num', 'lose_num', 'profile'];
 
     try {
       await this.createQueryBuilder()
         .insert()
-        .into(User, [
-          'id',
-          'password',
-          'rating',
-          'win_num',
-          'lose_num',
-          'profile',
-        ])
+        .into(User, keys)
         .values({
           id,
           password: encryptedPassword,
