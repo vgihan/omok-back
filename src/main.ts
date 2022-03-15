29802330b1897ec993d5483c91876607569baa72
vgihan <jum654@naver.com>
2022-03-15 15:22:33 +0900

    fix: 실행 포트를 환경 변수를 통해 지정하도록 수정

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.FRONT_SERVER });
-  await app.listen(5000);
+  await app.listen(process.env.API_SERVER_PORT);
 }
 bootstrap();
