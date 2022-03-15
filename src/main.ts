8e7191771b75fea754450f3da39db8151aabdf32
vgihan <jum654@naver.com>
2022-03-15 18:16:22 +0900

    fix: cors 삭제
    - 프록시를 통해 SOP를 처리하도록 수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: process.env.FRONT_SERVER });
   await app.listen(process.env.API_SERVER_PORT);
 }
 bootstrap();
